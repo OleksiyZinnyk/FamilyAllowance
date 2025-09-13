1a217d9609bda4ad99bbc2d3f9e84a14402d6c6b
OleksiyZinnyk <zniniklehaa@gmail.com>
2025-09-13 21:22:55 +0300

    Extends

--- FamilyAllowance/src/studentorder/SaveStudentOrder.java
+++ FamilyAllowance/src/studentorder/SaveStudentOrder.java
@@ -1,14 +1,11 @@
 package studentorder;
 
+import studentorder.domain.Adult;
 import studentorder.domain.StudentOrder;
 
 public class SaveStudentOrder {
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.hFirstName = "Olekisy";
-        so.hLastName = "Zinnyk";
-        so.wFirstName = "Masha";
-        so.wLastName = "Zinnyk";
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
@@ -16,7 +13,15 @@ public class SaveStudentOrder {
 
     static long saveStudentOrder(StudentOrder studentOrder) {
         long answer = 199;
-        System.out.println("saveStudentOrder: " + studentOrder.hFirstName);
+        System.out.println("saveStudentOrder: ");
         return answer;
+    }
+
+    StudentOrder buildStudentOrder() {
+        StudentOrder so = new StudentOrder();
+        Adult husband = new Adult();
+        husband.setGivenName("Oleksiy");
+        so.setHusband(husband);
+        return so;
     }
 }
